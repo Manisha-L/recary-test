62a2fa82a7801a03c9e981ec3c2c3e012f842734
Manisha-L <manisha.lakshmipathula3@gmail.com>
2021-12-01 11:33:26 +0000

    front-end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState, useRef } from "react";
+import { AgGridColumn, AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import "ag-grid-enterprise";
 
-function App() {
+const App = () => {
+  const [rowData, setRowData] = useState([]);
+
+  const gridRef = useRef(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/")
+      .then((rowData) => setRowData(rowData.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const ImageFormatter = ({ value }) => {
+    return <img src={value} />;
+  };
+
+  const onButtonClick = (e) => {
+    const selectedNodes = gridRef.current.api.getSelectedNodes();
+    const selectedData = selectedNodes.map((node) => node.data);
+    const selectedDataStringPresentation = selectedData
+      .map((node) => `${node.Identification} ${node.Name}`)
+      .join(", ");
+    alert(`Selected nodes: ${selectedDataStringPresentation}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ag-theme-alpine" style={{ height: "90vh", width: "100vw" }}>
+      <button onClick={onButtonClick}>Get selected rows</button>
+      <AgGridReact
+        ref={gridRef}
+        rowData={rowData}
+        rowSelection="multiple"
+        groupSelectsChildren={true}
+        pagination={true}
+        paginationAutoPageSize={true}
+        // autoGroupColumnDef={{
+        //   headerName: "Identification",
+        //   field: "Identification",
+        //   // cellRenderer: "agGroupCellRenderer",
+        //   cellRendererParams: {
+        //     checkbox: true,
+        //   },
+        // }}
+      >
+        <AgGridColumn
+          field="Identification"
+          sortable={true}
+          filter={true}
+          checkboxSelection={true}
+        ></AgGridColumn>
+        <AgGridColumn
+          field="SequenceNumber"
+          sortable={true}
+          filter={true}
+        ></AgGridColumn>
+        <AgGridColumn
+          field="Name"
+          sortable={true}
+          filter={true}
+          checkboxSelection={true}
+        ></AgGridColumn>
+        <AgGridColumn
+          field="Type"
+          sortable={true}
+          filter={true}
+          checkboxSelection={true}
+        ></AgGridColumn>
+        <AgGridColumn
+          field="Photo"
+          // imageUrl="http://www.barclays.co.uk/branchImages/content/dam/branchfinder/bedfordshire/UK-B-20533008.jpg"
+          sortable={true}
+          filter={true}
+          width="500"
+          checkboxSelection={true}
+          cellRendererFramework={ImageFormatter}
+        ></AgGridColumn>
+      </AgGridReact>
     </div>
   );
-}
+};
 
 export default App;
